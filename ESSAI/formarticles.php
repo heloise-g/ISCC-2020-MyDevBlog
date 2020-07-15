84cb810a1c38c95c16fbb70069e45979515039e2
heloise-g <heloise.gaspais@iseg.fr>
2020-07-15 18:40:01 +0200

    travail 15/07

--- ESSAI/formarticles.php
+++ ESSAI/formarticles.php
@@ -1,23 +1,45 @@
-<html>
-    <head>
-        <title>Blog Héloïse GASPAIS-contact</title>
-        <meta charset="utf-8">
-        <link type="text/css" rel="stylesheet" href="accueil.css">
-    </head>
+<head>
+    <title>Blog Héloïse GASPAIS-contact</title>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="accueil.css">
+</head>
 
+<?php
+session_start();
+$pdo = new PDO ("mysql:host=localhost; dbname=mydevblog", "root", "");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$titre= $_POST['titre'];
+$date= $_POST['date'];
+$auteur= $_POST['auteur'];
+$contenu= $_POST['contenu'];
+$extrait= $_POST['extrait'];
+
+if(isset($_POST["publication"])){
+
+    if(!empty('titre') && !empty('image')&& !empty('date')&& !empty('auteur')&& !empty('contenu')&& !empty('extrait')){
+        
+        $newarticle = $pdo->prepare('INSERT INTO articles(titre, date, auteur, contenu, extrait)VALUES(?,?,?,?,?,?)');
+        $newarticle->execute(array($titre, $date, $auteur, $contenu, $extrait));
+        echo $newarticle->fetch();
+        echo "Votre article a bien été publié";
+    } else{
+        echo "Tous les champs doivent être remplis !";
+    }
+}
+?>
     <div id="compte">
     <h2>Créez votre article ! A vous de jouer !</h2>
     </div>
 
     <body>
-        <form action="insertionArticles.php" method="post" enctype= "multipart/form-data">
+        <form action="" method="post" enctype= "multipart/form-data">
             <p>Titre de l'article: <input type="text" id="titre" name="titre" placeholder="Titre"></p>
             <br/>
             <br/>
                 <input type="file" id="image" name="image" placeholder="Image">
             <br/>
             <br/>
-                <input type="text" id="date" name="date" placeholder="Date de publication">
+                <input type="date" id="date" name="date" placeholder="Date de publication">
             <br/>
             <br/>
                 <input type="text" id="auteur" name="auteur" placeholder="Auteur">
@@ -32,7 +54,4 @@
                 <button type="submit" id="button" name="publication">Publier</button>
         </form>
     </body>
-
-
-</html>    
 
